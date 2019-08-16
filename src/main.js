b100e8218e641cc09bbb5f710518b0ef1f84843f
hWY <1045922393@qq.com>
2019-08-16 08:36:03 +0800

    原始VueCli

--- src/main.js
+++ src/main.js
@@ -1,45 +1,8 @@
 import Vue from 'vue'
-// import App from './App.vue'
-// import vueFirst from './components/first.vue'
-// import vtext from './components/v_text.vue'
-// import vhtml from './components/v-html.vue'
-// import vbind from './components/v_bind.vue'
-// import vfor from './components/v-for.vue'
-import vmodel from './components/vmodel.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = true
 
 new Vue({
-  render: h => h(vmodel)
+  render: h => h(App)
 }).$mount('#app')
-
-// new Vue({
-//   render: h => h(vfor)
-// }).$mount("#app")
-
-// new Vue({
-
-//   render: h => h(App)
-
-// }).$mount("#app")
-
-// new Vue({
-
-//   render: h => h(vbind)
-
-// }).$mount("#app")
-
-// // new Vue({
-//   render: h => h(vhtml)
-// }).$mount("#app")
-
-// new Vue({
-//   render: h => h(vueFirst),
-// }).$mount('#app')
-// new Vue({
-//   render: h => h(vtext)
-// }).$mount('#app')
-
-// new Vue({
-//   render: h => h(vueFirst),
-// }).$mount('#second')
